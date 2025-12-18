741adac11cea2d4ae3169a53d1a8f9d5b476208c
brunolegario <brunolegario@gmail.com>
2025-12-17 23:29:03 -0300

    agregation over time

--- src/commands/aggregate.ts
+++ src/commands/aggregate.ts
@@ -1,9 +1,60 @@
+import { getNextFeedToFetch, markFeedFetched } from "../lib/db/queries/feeds";
+import { Feed } from "../lib/db/schema";
 import { fetchFeed } from "../lib/rss";
+import { parseDuration } from "../lib/time";
 
-export async function handlerAgg(_: string) {
-  const feedURL = "https://www.wagslane.dev/index.xml";
+export async function handlerAgg(cmdName: string, ...args: string[]) {
+  if (args.length !== 1) {
+    throw new Error(`Usage: ${cmdName} <time_between_reqs>`);
+  }
 
-  const feedData = await fetchFeed(feedURL);
-  const feedDataString = JSON.stringify(feedData, null, 2);
-  console.log(feedDataString);
+  const time = args[0];
+  const timeBetweenReqs = parseDuration(time);
+  if (!timeBetweenReqs) {
+    throw new Error(
+      `Invalid duration format: ${time} — use format 1h 30m 15s or 3500ms`
+    );
+  }
+
+  console.log(`Starting aggregation every ${time}...`);
+
+  scrapeFeeds().catch(handleError);
+  const interval = setInterval(() => {
+    scrapeFeeds().catch(handleError);
+  }, timeBetweenReqs);
+
+  await new Promise<void>((resolve) => {
+    process.on("SIGINT", () => {
+      console.log("Shutting down feed aggregation...");
+      clearInterval(interval);
+      resolve();
+    });
+  });
+}
+
+export async function scrapeFeeds() {
+  const feed = await getNextFeedToFetch();
+  if (!feed) {
+    console.log("No feeds to fetch.");
+    return;
+  }
+
+  console.log(`Fetching feed: ${feed.name}`);
+  await scrapeFeed(feed);
+}
+
+export async function scrapeFeed(feed: Feed) {
+  await markFeedFetched(feed.id);
+
+  const feedData = await fetchFeed(feed.url);
+
+  console.log(
+    `Feed ${feed.name} collected, ${feedData.channel.item.length} posts found`
+  );
+}
+
+function handleError(err: unknown) {
+  console.error(
+    `Error during aggregation: ${err instanceof Error ? err.message : err}`
+  );
 }
